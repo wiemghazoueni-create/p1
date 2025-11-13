0650ffb1231380cede84faf9ba5faf8987e9ebec
wiemghazoueni-create <wiemghazoueni@gmail.com>
2025-11-13 19:17:02 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("partez")
+})
+basic.forever(function () {
+	
+})
